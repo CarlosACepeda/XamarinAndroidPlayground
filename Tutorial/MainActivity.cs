dbcff9559a1da0ceadf243936fd5e5935e50300e
Carlos Cepeda <carlosalt5126@hotmail.es>
2018-05-09 17:19:31 -0500

    Agregar archivos de proyecto.

---
+++ Tutorial/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Tutorial
+{
+    [Activity(Label = "Tutorial", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Button button;
+        private TextView texto;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            button = FindViewById<Button>(Resource.Id.btnClick);
+            texto = FindViewById<TextView>(Resource.Id.txtTexto);
+
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            texto.Text = "Haz clicado el botón";
+            Toast.MakeText(this, "Mensaje 1", ToastLength.Short).Show();
+        }
+    }
+}
+
